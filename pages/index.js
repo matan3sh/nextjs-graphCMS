bd6c07230ba7e5c00d1e5075b7a12351c63a150b
Matan Shaviro <matan3sh@gmail.com>
2021-10-11 21:16:00 +0300

    Init project and fetching data from graphcms

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,46 @@
-const Home = () => {
+import { gql, GraphQLClient } from 'graphql-request';
+
+const Home = ({ videos }) => {
+  console.log(videos);
   return <div>Hello</div>;
+};
+
+export const getStaticProps = async () => {
+  const url = process.env.ENDPOINT;
+  const client = new GraphQLClient(url, {
+    headers: {
+      Authorization: process.env.GRAPH_CMS_TOKEN,
+    },
+  });
+
+  const query = gql`
+    query {
+      videos {
+        createdAt
+        id
+        title
+        description
+        seen
+        slug
+        tags
+        thumbnail {
+          url
+        }
+        mp4 {
+          url
+        }
+      }
+    }
+  `;
+
+  const data = await client.request(query);
+  const videos = data.videos;
+
+  return {
+    props: {
+      videos,
+    },
+  };
 };
 
 export default Home;
